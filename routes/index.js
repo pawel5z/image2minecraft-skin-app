19922fe6d3a2906bfac676f494dbb76156d8c7b1
Paweł Szmergała <pawelszme@gmail.com>
2022-08-24 22:03:00 +0200

    Implement displaying error messages

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ const exec = util.promisify(require('child_process').exec);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Image to Minecraft skin' });
+  res.render('index');
 });
 
 /* POST home page */
@@ -20,7 +20,7 @@ router.post(
   upload.fields([{ name: 'frontImg', maxCount: 1 }, { name: 'backImg', maxCount: 1 }]),
   async function (req, res, next) {
     if (!req.files.frontImg || !req.files.backImg)
-      return res.send('Some images are missing.').end();
+      return res.render('index', { formInputError: 'Some images are missing.' });
 
     var generatedSkinName = uuidv4() + '.png';
     try {
@@ -29,14 +29,14 @@ router.post(
         { cwd: 'image_processing' });
     } catch (err) {
       console.error(err);
-      res.send('Error creating skin.').end();
+      res.render('index', { formInputError: 'Error creating skin.' });
     }
 
     // Send generated skin to user.
     res.download(`image_processing/${generatedSkinName}`, 'skin.png', async function (err) {
       if (err) {
         console.error(err);
-        res.send('Error downloading skin.');
+        res.render('index', { formInputError: 'Error downloading skin.' });
       }
       try {
         await fs.promises.unlink(`image_processing/${generatedSkinName}`);
