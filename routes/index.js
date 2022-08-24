88c5b79a98c7066213abc3e326507d0b5cbf98d7
Paweł Szmergała <pawelszme@gmail.com>
2022-08-24 21:32:31 +0200

    Apply async/await pattern

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var multer = require('multer');
-var upload = multer({ dest: 'image_processing/upload' });
-var fs = require('fs');
-var child_process = require('child_process');
+const express = require('express');
+const fs = require('fs');
+const multer = require('multer');
+const router = express.Router();
+const upload = multer({ dest: 'image_processing/upload' });
 const { v4: uuidv4 } = require('uuid');
+
+const util = require('util');
+
+const exec = util.promisify(require('child_process').exec);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,37 +18,42 @@ router.get('/', function (req, res, next) {
 router.post(
   '/',
   upload.fields([{ name: 'frontImg', maxCount: 1 }, { name: 'backImg', maxCount: 1 }]),
-  function (req, res, next) {
+  async function (req, res, next) {
     if (!req.files.frontImg || !req.files.backImg)
-      return res.send('some images are missing').end();
+      return res.send('Some images are missing.').end();
 
     var generatedSkinName = uuidv4() + '.png';
-    child_process.execSync('python3 img2mc_skin/main.py skin_template.png'
-      + ' upload/' + req.files.frontImg[0].filename
-      + ' upload/' + req.files.backImg[0].filename
-      + ' -o ' + generatedSkinName,
-      { cwd: 'image_processing' });
+    try {
+      await exec(
+        `python3 img2mc_skin/main.py skin_template.png upload/${req.files.frontImg[0].filename} upload/${req.files.backImg[0].filename} -o ${generatedSkinName}`,
+        { cwd: 'image_processing' });
+    } catch (err) {
+      console.error(err);
+      res.send('Error creating skin.').end();
+    }
 
     // Send generated skin to user.
-    var file = fs.createReadStream('image_processing/' + generatedSkinName);
-    file.on('end', () => {
-      fs.unlink('image_processing/' + generatedSkinName, (err) => {
-        if (err) throw err;
-        console.log(generatedSkinName + ' has been deleted');
-      });
+    res.download(`image_processing/${generatedSkinName}`, 'skin.png', async function (err) {
+      if (err) {
+        console.error(err);
+        res.send('Error downloading skin.');
+      }
+      try {
+        await fs.promises.unlink(`image_processing/${generatedSkinName}`);
+      } catch (err) {
+        console.error(err);
+      }
     });
-    res.header('Content-Disposition', 'attachment; filename="skin.png"');
-    file.pipe(res);
 
     // Delete uploaded files.
-    fs.unlink(req.files.frontImg[0].path, (err) => {
-      if (err) throw err;
-      console.log(req.files.frontImg[0].filename + ' was deleted');
-    });
-    fs.unlink(req.files.backImg[0].path, (err) => {
-      if (err) throw err;
-      console.log(req.files.backImg[0].filename + ' was deleted');
-    });
+    try {
+      await Promise.all([
+        fs.promises.unlink(req.files.frontImg[0].path),
+        fs.promises.unlink(req.files.backImg[0].path)
+      ]);
+    } catch (err) {
+      console.error(err);
+    }
   }
 );
 
